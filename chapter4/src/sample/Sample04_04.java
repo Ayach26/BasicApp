b9c0ad1b33f03bb6c38165e9eb456db346d54e94
Ayaka Fukuda <f26ayach@gmail.com>
2021-08-15 16:24:38 +0900

    演算子と演算復習

---
+++ chapter4/src/sample/Sample04_04.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Sample04_04 {
+
+  public static void main(String[] args) {
+    String s = "こんにちわ" + "太郎" + "さん";
+    System.out.println(s);
+
+    String name = "太郎";
+    System.out.println("こんにちわ" + name + "さん");
+  }
+}
